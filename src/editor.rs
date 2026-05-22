9e83d9609cf24fde4d996cc60cb52b7a895cce8c
Simon Pollak <ludoop@telekom.ip>
2026-05-22 14:09:48 +0200

    editor quits when ctrl + q is pressed

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,9 @@
-use crossterm::event::{Event::Key, KeyCode::Char, read};
+use crossterm::event::{Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     /*
@@ -14,32 +16,55 @@ impl Editor {
     pub fn default() -> Self {
         // In Rust: the last line of a block is returned if it does not end with a semicolon
         // Here the last line returns an Editor instance, empty
-        Editor {}
+        Editor { should_quit: false }
+    }
+
+    // mut indicated that we will be modifying the reference
+    pub fn run(&mut self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
     }
 
     // Self is the same as this in java
     // &self means that the function takes a reference to an instance of struct it is implemented on,
     // in this case, an Editor instance
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
+    // -> Result<(), std::io::Error> means that the function returns a Result type,
+    //  either Ok with nothing or Err with a std::io::Error in it
+    pub fn repl(&mut self) -> Result<(), std::io::Error> {
+        // ? unwraps the result, returning the error if it is Err, or continuing if it is Ok
+        enable_raw_mode()?;
         // loop continues until break is encountered
         loop {
-            match read() {
-                // this says, if read returns OK and inside is a Key, process event in the "Key box"
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
+            // we take keyevent and destructure it into its fields
+            if let Key(KeyEvent {
+                code,
+                modifiers,
+                kind,
+                state,
+            }) = read()?
+            {
+                println!(
+                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r"
+                );
+                match code {
+                    // we are only matching if further the modifiers are control, so 'q' is only quit if ctrl-q is pressed
+                    Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
                     }
+                    _ => (),
                 }
-                // {:?} prints the debug representation of the error
-                Err(err) => println!("Error: {err}"),
-                _ => (),
+            }
+            if self.should_quit {
+                break;
             }
         }
-
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        // Our function now needs to return something.
+        // We leverage the fact that Rust treats the last line as something we want to return (notice the missing ;).
+        // If we made it down here, it means none of the functions with a ? have found an error that has been returned, so we can safely return a pink box labeled Ok with nothing in it.
+        // The code representation of this is: Ok(()).
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
-    // no need to specify the struct reference
-    // when called on a specific instance, rust automatically
-    // dereferences the instance to call the method
-    // editor.run() is equivalent to (&editor).run()
-    editor.run();
+    // now we are running the run() function output of default(), which is an Editor instance
+    Editor::default().run();
 }
