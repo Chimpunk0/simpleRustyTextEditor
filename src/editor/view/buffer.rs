b2c223a203487117b03ac8ea38e79a5e9d0e3bb1
Simon Pollak <ludoop@telekom.ip>
2026-06-03 18:49:44 +0200

    assignment 10, scrolling

--- src/editor/view/buffer.rs
+++ src/editor/view/buffer.rs
@@ -1,23 +1,19 @@
 use std::fs::read_to_string;
 use std::io::Error;
 
+use super::line::Line;
+
+#[derive(Default)]
 pub struct Buffer {
-    pub lines: Vec<String>,
+    pub lines: Vec<Line>,
 }
 
-impl Default for Buffer {
-    fn default() -> Self {
-        Self {
-            lines: vec!["Hello, World!".to_string()],
-        }
-    }
-}
 impl Buffer {
     pub fn load(file_name: &str) -> Result<Self, Error> {
         let contents = read_to_string(file_name)?;
         let mut lines = Vec::new();
         for value in contents.lines() {
-            lines.push(String::from(value));
+            lines.push(Line::from(value));
         }
         Ok(Self { lines })
     }
