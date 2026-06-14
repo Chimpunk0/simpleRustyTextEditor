706d912a9ce2dbb467b4dbdd1952b81bb327a961
Simon Pollak <ludoop@telekom.ip>
2026-06-14 19:31:33 +0200

    start implement simple message bar, code refactor at the same time

--- src/editor.rs
+++ src/editor.rs
@@ -7,23 +7,30 @@ use std::{
 mod documentstatus;
 mod editorcommand;
 mod fileinfo;
+mod messagebar;
 mod statusbar;
 mod terminal;
+mod uicomponent;
 mod view;
 
 use documentstatus::DocumentStatus;
 use editorcommand::EditorCommand;
 use statusbar::StatusBar;
 use terminal::Terminal;
+use uicomponent::UIComponent;
 use view::View;
 
+use self::{messagebar::MessageBar, terminal::Size};
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Editor {
     should_quit: bool,
     view: View,
     status_bar: StatusBar,
+    message_bar: MessageBar,
+    terminal_size: Size,
     title: String, // title of the terminal
 }
 
@@ -35,21 +42,37 @@ impl Editor {
             current_hook(panic_info);
         }));
         Terminal::initialize()?;
-        let mut editor = Self {
-            should_quit: false,
-            view: View::new(2),
-            status_bar: StatusBar::new(1),
-            title: String::new(),
-        };
+        let mut editor = Self::default();
+        let size = Terminal::size().unwrap_or_default();
+        editor.resize(size);
         let args: Vec<String> = env::args().collect();
         if let Some(file_name) = args.get(1) {
             editor.view.load(file_name);
         }
+        editor
+            .message_bar
+            .update_message("HELP: Ctrl-S = save | Ctrl-Q = quit".to_string());
         editor.refresh_status();
         Ok(editor)
     }
 
-    pub fn refresh_status(&mut self) {
+    fn resize(&mut self, size: Size) {
+        self.terminal_size = size;
+        self.view.resize(Size {
+            height: size.height.saturating_sub(2),
+            width: size.width,
+        });
+        self.message_bar.resize(Size {
+            height: 1,
+            width: size.width,
+        });
+        self.status_bar.resize(Size {
+            height: 1,
+            width: size.width,
+        });
+    }
+
+    fn refresh_status(&mut self) {
         let status = self.view.get_status();
         let title = format!("{} - {NAME}", status.file_name);
         self.status_bar.update_status(status);
